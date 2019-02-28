ad25ab767508538550fe41b24777c4c93067eeb1
Christian Parpart <christian@parpart.family>
2019-03-01 00:42:38 +0100

    experimental symbolics WIP commit

--- src/Fetisch/Symbolics.fs
+++ src/Fetisch/Symbolics.fs
@@ -78,16 +78,47 @@ type Expression =
             | Power (b, e) ->
                 sprintf "%s^%s" (embrace b) (embrace e)
 
+module Patterns =
+    open FSharp.Core.LanguagePrimitives
+
+    let (|Zero|_|) = function
+        | Number n when n.IsZero -> Some Zero
+        | _ -> None
+
+    let (|One|_|) = function
+        | Number n when n.IsOne -> Some One
+        | _ -> None
+
+    let (|Positive|_|) = function
+        | Number n when n > GenericZero -> Some Positive
+        | Constant E | Constant Pi -> Some Positive
+        | _ -> None
+
+    let (|Negative|_|) = function
+        | Number n when n < GenericZero -> Some Negative
+        | _ -> None
+
+    let (|Terminal|_|) = function
+        | Number _
+        | Variable _
+        | Constant _ as t ->
+            Some t
+        | _ ->
+            None
+
 module Operations =
+    open Patterns
+
     let zero = Number(BigRational.Zero)
     let one = Number(BigRational.One)
     let minusOne = Number(BigRational.MinusOne)
 
-    let variable (name: string) = Variable name
     let number n = Number n
+    let variable (name: string) = Variable name
+    let var = variable
 
-    //let isZero = function | zero -> true | _ -> false
-    //let isOne = function | one -> true | _ -> false
+    let isZero = function | Zero -> true | _ -> false
+    let isOne  = function | One  -> true | _ -> false
 
     // Order-relation (<) between two expressions.
     let internal orderRelation (x: Expression) (y: Expression): bool =
@@ -102,65 +133,170 @@ module Operations =
             | Variable x, Variable y -> x < y
             | Variable _, _ -> true
             | _, Variable _ -> false
-            | Sum xs, Sum ys -> zippedRelation xs ys
-            | Sum xs, y -> zippedRelation xs [y]
-            | x, Sum ys -> zippedRelation [x] ys
+            | Function (fx, x), Function (fy, y) -> if fx <> fy then fx < fy else relation x y
+            | Power (xr, xp), y -> if xr <> y then relation xr y else relation xp one
+            | x, Power (yr, yp) -> if x <> yr then relation x yr else relation one yp
             | Product xs, Product ys -> zippedRelation xs ys
             | Product xs, y -> zippedRelation xs [y]
             | x, Product ys -> zippedRelation [x] ys
-            | _ -> false
+            | Sum xs, Sum ys -> zippedRelation xs ys
+            | Sum xs, y -> zippedRelation xs [y]
+            | x, Sum ys -> zippedRelation [x] ys
         and zippedRelation a b =
             match a, b with
             | x::xs, y::ys when x <> y -> relation x y
             | x::xs, y::ys -> zippedRelation xs ys
-            | _ -> false
+            | [], y::ys -> true
+            | _, [] -> false
         relation x y
 
     // Creates the Sum of two summands.
-    let add (x: Expression) (y: Expression): Expression =
-        let merge ax bx =
-            ax @ bx
+    let rec add (x: Expression) (y: Expression): Expression =
+        printfn "Add: x=%A, y=%A" x y
+        let (|Term|_|) = function
+            | Number _ -> None
+            | Product [a; b] -> Some (a, b)
+            | Product (a :: rest) -> Some (a, Product rest)
+            | x -> Some (one, x)
+        let sum (a: Expression, b: Expression): Expression =
+            printfn "Add.Sum: a=%A, b=%A" a b
+            match a, b with
+            | Number a', Number b' -> Number(a' + b')
+            | _ -> Sum([a; b])
+        let merge (u: Expression list) (v: Expression list): Expression =
+            printfn "Add.Merge: u=%A, v=%A" u v
+            let rec fold (acc: Expression list) (u: Expression list) (v: Expression list): Expression list =
+                printfn "Add.Merge.Fold: acc=%A, u=%A, v=%A" acc u v
+                match acc, u, v with
+                | Zero::acc', _, _ ->
+                    printfn "  match (Zero::acc, _, _): acc'=%A" acc'
+                    fold acc' u v
+                | Term(ac, at)::acc', Term(xc, xt)::xs, y when at = xt ->
+                    printfn "  match ac: %A; xc: %A" ac xc
+                    fold ((mul (sum (ac, xc)) at)::acc') xs y
+                | Term(ac, at)::acc', x, Term(yc, yt)::ys when at = yt ->
+                    printfn "  match ac: %A; yc: %A" ac yc
+                    fold ((mul (sum (ac, yc)) at)::acc') x ys
+                | _, Term(xc, xt)::xs, Term(yc, yt)::ys when xt = yt ->
+                    // (a*x + p) + (b*y + q) = (a+b)*x + p + q
+                    printfn "match (_, xc: %A; yc: %A)" xc yc
+                    fold ((mul (sum (xc, yc)) xt)::acc) xs ys
+                | _, x::xs, y::ys ->
+                    printfn "  match (_, x::xs, y::ys): x=%A, xs=%A, y=%A, ys=%A" x xs y ys
+                    if orderRelation x y
+                    then fold (x::acc) xs v
+                    else fold (y::acc) u ys
+                | _, x::xs, [] ->
+                    printfn "  match (_, x::xs, []): x=%A, xs=%A" x xs
+                    fold (x::acc) xs []
+                | _, [], y::ys ->
+                    printfn "  match (_, [], y::ys): y=%A, ys=%A" y ys
+                    fold (y::acc) [] ys
+                | _, [], [] ->
+                    printfn "  match (_, [], [])"
+                    acc
+            match fold [] u v with
+            | []  -> zero
+            | [x] -> x
+            | x   -> Sum (List.rev x)
         match x, y with
+        | Zero, _ -> y
+        | _, Zero -> x
+        | Number a, Number b ->
+            printfn "Add.match (num, num): a=%A, b=%A" a b
+            Number (a + b)
+        | Sum (Number(a)::ax), Sum (Number(b)::bx) ->
+            printfn "Add.match (Sum(Num(a)::ax), Sum(Num(b)::bx): a=%A, ax=%A, b=%A, bx=%A" a ax b bx
+            add (Number(a + b)) (merge ax bx)
+        | Sum (Number(a)::ax), Number b ->
+            printfn "Add.match (Sum(Num(a)::ax) Num(b): a=%A, ax=%A, b=%A" a ax b
+            add (Number (a + b)) (Sum ax)
         | Sum(ax), Sum(bx) ->
-            Sum(merge ax bx)
+            printfn "Add.match (Sum(ax), Sum(bx)): ax=%A, bx=%A" ax bx
+            merge ax bx
         | Sum(ax), b ->
-            Sum(merge ax [b])
+            printfn "Add.match (Sum(ax), b): ax=%A, b=%A" ax b
+            merge ax [b]
         | a, Sum(bx) ->
-            Sum(merge [a] bx)
+            printfn "Add.match (a, Sum(bx)): a=%A, bx=%A" a bx
+            merge [a] bx
         | a, b ->
-            Sum(merge [a] [b])
+            printfn "Add.match (a, b): a=%A, b=%A" a b
+            merge [a] [b]
 
     // Creates the product of two factors.
-    let mul (a: Expression) (b: Expression): Expression =
-        let merge ax bx =
-            ax @ bx
-        match a, b with
+    and mul (x: Expression) (y: Expression): Expression =
+        let (|Term|_|) = function
+            | Number _ -> None
+            | Power(a, b) -> Some (a, b)
+            | any -> Some (any, one)
+
+        let merge (u: Expression list) (v: Expression list): Expression =
+            let rec fold (acc: Expression list) (u: Expression list) (v: Expression list): Expression list =
+                match acc, u, v with
+                | One::acc', _, _ ->
+                    fold acc' u v
+                | Term(ab,ae)::acc', Term(xb,xe)::xs, y
+                | Term(ab,ae)::acc', y, Term(xb,xe)::xs when ab = xb ->
+                    fold ((pow ab (add ae xe))::acc') xs y
+                | _, Term(xb,xe)::xs, Term(yb,ye)::ys when xb = yb ->
+                    fold ((pow xb (add xe ye))::acc) xs ys
+                | _, x::xs, y::ys ->
+                    if orderRelation x y
+                    then fold (x::acc) xs v
+                    else fold (y::acc) u ys
+                | _, x::xs, y -> fold (x::acc) xs y
+                | _, [], y::ys -> fold (y::acc) ys []
+                | _, [], [] -> acc
+            match fold [] u v with
+            | []  -> one
+            | [t] -> t
+            | s   -> Product(List.rev s)
+
+        match x, y with
+        | One, a | a, One -> a
+        | Zero, _ | _, Zero -> zero
+        | Number a, Number b ->
+            Number (a * b)
+        | Product (Number(a)::ax), Product (Number(b)::bx) ->
+            mul (Number(a * b)) (merge ax bx)
         | Product(ax), Product(bx) ->
-            Product(merge ax bx)
-        | Product(ax), b ->
-            Product(merge ax [b])
-        | a, Product(bx) ->
-            Product(merge [a] bx)
+            merge ax bx
+        | Product(ax), _ ->
+            merge ax [y]
+        | _, Product(bx) ->
+            merge [x] bx
         | _ ->
-            Product(merge [a] [b])
+            merge [x] [y]
+
+    and pow (x: Expression) (y: Expression) =
+        match x, y with
+        | _, Zero -> one
+        | _, One -> x
+        | Number ax, Number bx ->
+            Number(ax ** bx)
+        | Product ax, Number b when b.IsInteger ->
+            Product(ax |> List.map (fun z -> pow z y))
+        | Power (r, p), Number b when b.IsInteger -> pow r (mul p y)
+        | _, _ -> Power(x, y)
 
     // Constructs additive inverse.
     let negate (x: Expression) = mul minusOne x
+
+    let sub (x: Expression) (y: Expression): Expression =
+        add x (negate y)
 
     // Constructs multiplicative inverse.
     let invert = function
         | Number x -> Number(1N / x)
         | x -> Power(x, minusOne)
 
-    let sub (x: Expression) (y: Expression): Expression =
-        add x (negate y)
-
     let div (x: Expression) (y: Expression) =
         mul x (invert y)
 
 type Expression with
-    static member Zero = Number(BigRational.Zero)
-    static member One = Number(BigRational.One)
+    static member Zero = Operations.zero
+    static member One = Operations.one
 
     static member FromInt (x: int) = Number(BigRational.FromInt x)
     static member FromBigInt (x: bigint) = Number(BigRational.FromBigInt x)
@@ -170,19 +306,28 @@ type Expression with
     static member ( * ) (a: Expression, b: Expression) = Operations.mul a b
     static member ( / ) (a: Expression, b: Expression) = Operations.div a b
 
-    static member Pow (a: Expression, b: Expression) = Power(a, b)
+    static member ( ~- ) (a: Expression) = Operations.sub Operations.zero a
+    static member ( ~+ ) (a: Expression) = a
+
+    static member Pow (a: Expression, b: Expression) = Operations.pow a b
     static member Abs (a: Expression) = Function(Function.Abs, a)
 
 [<RequireQualifiedAccess>]
 module NumericLiteralG =
-    let FromZero() = Expression.Zero
-    let FromOne() = Expression.One
+    let FromZero() = Operations.zero
+    let FromOne() = Operations.one
     let FromInt32 (x: int32) = x |> Expression.FromInt
     let FromInt64 (x: int64) = Expression.FromBigInt (bigint x)
     let FromString (x: string) = bigint.Parse x |> Expression.FromBigInt
 
 module Test =
+    open Operations
     let main () =
-        let d = ((2G + 3G) * 4G) ** 5G / 6G
+        let x = var "x"
+        //let d = (x * (2G + 3G) * 4G) ** 2G
+        //let d = x * (x ** (3G + x))
+        //let d = 1G + (3G + x)
+        //let d = (2G + x) + 1G
+        let d = (2G + 3G) + 4G
         printfn "d: %A" d
 
