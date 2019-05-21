35fcc04edc4b3987261a67968a31c9753a6f706d
Christian Parpart <christian@parpart.family>
2019-05-22 00:59:59 +0200

    some more BigRational tests

--- src/Fetisch.Test/Test.BigRational.fs
+++ src/Fetisch.Test/Test.BigRational.fs
@@ -11,6 +11,9 @@ open System
 open Xunit
 open Fetisch
 
+let inline f a b =
+    BigRational.FromIntFraction(a, b)
+
 [<Fact>]
 let ``creation & ops`` () =
     let a = 2N
@@ -19,10 +22,39 @@ let ``creation & ops`` () =
     Assert.Equal(1I, a.Denominator)
 
 [<Fact>]
-let ``ops`` () =
+let ``add`` () =
+    // a/b + c/d = (ad + bc) / bd
+    Assert.Equal(f 5 4, (f 1 2) + (f 3 4))
+
+[<Fact>]
+let ``sub`` () =
     let a = 2N
     Assert.Equal(0N, a - a)
+
+[<Fact>]
+let ``mul`` () =
+    // a/b * c/d
+    Assert.Equal(f 3 10, (f 1 2) * (f 3 5))
+
+[<Fact>]
+let ``div`` () =
     Assert.Equal(1N, a / a)
     Assert.Equal(1N / 2N, (a / a) / a)
 
+[<Fact>]
+let ``pow`` () =
+    let a = BigRational.FromIntFraction(1, 2)
+    let b = BigRational.FromIntFraction(3, 4)
+
+    // (a/b)^0 = 1
+    Assert.Equal(1N, 5N ** BigRational.Zero)
+
+    // (a/b)^1 = a/b
+    Assert.Equal(5N, 5N ** BigRational.One)
+
+    // (a/b)^(-a/b) = 1
+    Assert.Equal(BigRational.One, (a/b) ** (-a/b))
+
+[<Fact>]
+let ``abs`` () =
     Assert.Equal(2N, abs -2N)
